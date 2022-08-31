760dd37428080d279d8e07325a7ab343108190b4
Love <lovesoni607@gmail.com>
2022-08-31 21:56:31 +0530

    Playground

---
+++ Playground/01VariablesAndConst.playground/Contents.swift
@@ -0,0 +1,7 @@
+var userName :String = "love soni"
+let rollNo :Float = 5
+print(userName)
+userName="love"
+print("Roll No is :",rollNo,userName);
+
+var name,email,password: String
---
+++ Playground/HelloWorld.playground/Contents.swift
@@ -0,0 +1,4 @@
+
+
+var greeting = "Hello, love"
+print(greeting)
