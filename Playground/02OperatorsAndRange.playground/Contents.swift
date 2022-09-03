b355a3a4d10fbdcc163f059731c64b2110ee93de
Love <lovesoni607@gmail.com>
2022-09-03 13:56:25 +0530

    Operators and range

---
+++ Playground/02OperatorsAndRange.playground/Contents.swift
@@ -0,0 +1,15 @@
+var result = 4==3
+print(result)
+var result1 = 4+3
+print(result1)
+var result2 = 4-3
+print(result2)
+var result3 = 4/3
+print(result3)
+var result4 = 4%3
+print(result4)
+
+var onesiderange = 1...5
+print(onesiderange.contains(5))
+var closedRange = 1..<5
+print(closedRange.contains(5))
